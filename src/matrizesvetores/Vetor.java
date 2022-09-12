7d2efa0c5c36ecbfa2c0787d7063d065744f80b8
juliana035 <juliana.ferreira_cruz@yahoo.com.br>
2022-09-12 20:54:55 -0300

    correcao e matriz e vetores

--- src/matrizesvetores/Vetor.java
+++ src/matrizesvetores/Vetor.java
@@ -1,5 +1,7 @@
 package matrizesvetores;
+
 import java.util.Scanner;
+
 /*2- Faça um programa que receba 6 números inteiros e mostre: 
 • Os números pares digitados;  
 • A soma dos números pares digitados; 
@@ -10,50 +12,40 @@ import java.util.Scanner;
 public class Vetor {
 
 	public static void main(String[] args) {
-		
-		 int[] num = new int [6];
-	     
-		 int [] numPar = new int[6];
-		 
-		 int [] numImpar = new int[6];
-		 
-	     int somaPar=0,contImpar=0;
-	     
-		 int x;
-	     
-	     
-		Scanner leia= new Scanner (System.in);
-        				
-         for(x=0;x<6;x++){
-        	System.out.println("\nEntre com o Número:");
-        	num [x]= leia.nextInt();
-        	
-         if (num [x] % 2 == 0) {
- 					
- 				somaPar+= num[x];
- 			    //numPar[x]= leia.nextInt();
- 			
-              }else {
-              contImpar++;
-            // numImpar = num[x];	 
-              
+
+		int[] num = new int[6];
+
+		int somaPar = 0, contImpar = 0;
+
+		int x;
+
+		Scanner leia = new Scanner(System.in);
+
+		for (x = 0; x < 6; x++) {
+			System.out.println("\nEntre com o Número:");
+			num[x] = leia.nextInt();
+
+			if (num[x] % 2 == 0) {
+
+				somaPar += num[x];
+
+			} else {
+				contImpar++;
+
+			}
+
+		}
+
+		System.out.println("\nSoma dos Numeros Pares:" + somaPar);
+		System.out.println("\nQuantidade Numeros Impar:" + contImpar);
+		System.out.println("\nNúmeros Digitados:");
+		int i;
+		for (i = 0; i < 6; i++) {
+			if (num[i] % 2 != 0) {
+				System.out.println("Número Impar:" + num[i]);
+			} else {
+				System.out.println("Número Par:" + num[i]);
+			}
+		}
+	}
 }
-        
-         }
-         
-          System.out.println("\nSoma dos Numeros Pares:"+somaPar);
-          System.out.println("\nQuantidade Numeros Impar:"+contImpar);
-          System.out.println ("\nNúmeros Digitados:");
-	       int i;
-           for(i=0;i<6;i++) {
-	             if(num [i] % 2 != 0) {
-	    	 System.out.println("Número Impar:"+ num[i]);   
-	    	   }else {
-	    	   System.out.println("Número Par:"+ num[i]);
-	    	   }
-	       }
-}	      
-}
-  
-          
-           
